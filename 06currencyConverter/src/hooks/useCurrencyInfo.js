be300f47faeb0b6b2988df0e1b41893146b0d639
Rakesh Mehta <reetsnp@gmail.com>
2025-12-23 12:58:25 +0545

    currency converter project complete

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -5,12 +5,12 @@ import { useState, useEffect } from "react";
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect( () => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+
         .then( (res) => res.json())
         .then( (res) => setData(res[currency]))
     }, [currency])
 
-    console.log(data)
     return data
 }
 
