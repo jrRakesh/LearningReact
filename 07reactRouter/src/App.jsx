c726abf5ecc8377861ab72f31563c9a646058174
Rakesh Mehta <reetsnp@gmail.com>
2025-12-28 14:04:17 +0545

    react-router crash course

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
@@ -10,9 +9,6 @@ function App() {
 
   return (
     <>
-      <Header />
-     {/* <Footer /> */}
-     18:00
     </>
   )
 }
---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/jrRakesh')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    // }, [])
+  return (
+    <div className='bg-gray-600 text-3xl text-white p-4'>Github Followers : {data.followers}
+    <img src={data.avatar_url} alt="Github_image" />
+    </div>
+
+  )
+}
+
+export default Github
+
+export const GithubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/jrRakesh')
+    return response.json() 
+}
