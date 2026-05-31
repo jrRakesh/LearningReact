196f5b3bc149d91a8016a3921a0b9a61813820eb
Rakesh Mehta <reetsnp@gmail.com>
2026-05-31 20:34:14 +0545

    creating contextAPI

--- 10contextTODO/src/App.jsx
+++ 10contextTODO/src/App.jsx
@@ -1,16 +1,31 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { TodoProvider } from './context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => { [...prev, {id: Date.now(),...todo}]})
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>TODO Notes</h1>
-    </>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+
+    </TodoProvider>
   )
 }
 
