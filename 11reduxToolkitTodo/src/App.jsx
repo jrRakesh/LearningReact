b2a9cf49a6f0231f2687a68ac2f2193949aa9291
Rakesh Mehta <125866161+jrRakesh@users.noreply.github.com>
2026-06-08 10:25:51 +0000

    Learning ReduxToolkit

--- 11reduxToolkitTodo/src/App.jsx
+++ 11reduxToolkitTodo/src/App.jsx
@@ -1,12 +1,15 @@
 
-import './App.css'
+
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   
-
   return (
     <>
-      <h1>hey there</h1>
+      <h1>Learning about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
