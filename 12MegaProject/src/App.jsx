cdaed84c339bdb9eaa29cbe75535fca4dfe7701f
Rakesh Mehta <reetsnp@gmail.com>
2026-06-09 23:20:29 +0545

    Initialized the mega project with .env file and appwrite configurations

---
+++ 12MegaProject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+     <h1>Blog App using AppWrite</h1>
+    </>
+  )
+}
+
+export default App
