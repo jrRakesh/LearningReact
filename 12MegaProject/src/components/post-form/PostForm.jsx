41f31dfb3d204a9294f8fb7d6128f40ad6d157c7
jrRakesh <reetsnp@gmail.com>
2026-07-10 22:40:11 +0545

    adding slug values

--- 12MegaProject/src/components/post-form/PostForm.jsx
+++ 12MegaProject/src/components/post-form/PostForm.jsx
@@ -2,10 +2,38 @@ import React, {useCallback} from 'react'
 import {useForm} from 'react-hook-form'
 import {Button, Input, RTE, Select } from "..index"
 import appwriteService from "../../appwrite/config";
-import { useNavigate } from "react-router-dom";
+import { data, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-function PostForm() {
+function PostForm({post}) {
+  const {register, handleSubbmit, watch, setValue, control, getValue} = useForm({defaultValues: {
+    title: post?.title || "",
+    slug: post?.slug || "",
+    content: post?.content || "",
+    status: post?.status || "active"
+    
+  },})
+  const navigate = useNavigate()
+  const userData = useSelector(state=> state.user.userData)
+  const submit = async (data) => {
+    if (post){
+      const file = data.image[0] ? appwriteService.uploadFile(data.image[0]): null
+      if(file){
+        appwriteService.deleteFile(post.featuredImage)
+      }
+      const dbPost = await appwriteService.updatePost(
+        post.$id, {
+          ...data,
+          featuredImage: file? file.$id : undefined
+        }
+      )
+      if (dbPost){
+        navigate(`/post/${dbPost.$id}`)
+      }
+    } else {
+      
+    }
+  }
   return (
     <div>PostForm</div>
   )
